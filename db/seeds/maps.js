af9e7fbf42515844e1cea538a451e52645ab5c9f
Nic <nic.ehmayer92@gmail.com>
2018-02-09 00:04:22 +0000

    Fixed typo in migration

--- db/seeds/maps.js
+++ db/seeds/maps.js
@@ -6,8 +6,7 @@ exports.seed = function(knex, Promise) {
         // Inserts seed entries
         knex('maps').insert({id: 1, name: 'Food', description: 'Good places to eat', x: 51.04489, y: -114.04443, user_id: 2}),
         knex('maps').insert({id: 2, name: 'Views', description: 'Great views around the city', x: 51.14498, y: -114.14498, user_id: 1}),
-        knex('maps').insert({id: 3, name: 'Parks', description: 'Awesome parks', x: 50.04432, y: -114.14473\q
-          , user_id: 3})
+        knex('maps').insert({id: 3, name: 'Parks', description: 'Awesome parks', x: 50.04432, y: -114.14473, user_id: 3})
       ]);
     });
 };
