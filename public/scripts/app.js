83189b59ab046d48bf4d8f388fe00e2a7f2df5ae
cocomatt <matt.currie@outlook.com>
2018-02-10 19:38:45 +0000

    Can render two maps, one of which has markers

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,5 @@
 
-function initMap(markers, description) {
+function initIndexMaps(markers, description) {
   const  map1 = L.map('map1').setView([51.045961 , -114.069135], 13);      //zoom 10 first whole city in view, 20 zoom in all the way
 
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
@@ -16,14 +16,14 @@ const  map2 = L.map('map2').setView([51.045961 , -114.069135], 20);      //zoom 
   // --- Renders all markers from markers table to map ---
   markers.forEach(function(val, index) {
 
-    L.marker(markers[index].latlng).addTo(map)
+    L.marker(markers[index].latlng).addTo(map1)
         .bindPopup(markers[index].description)
         .openPopup();
   })
 
   map.on('click', (e) => {
     console.log(e.latlng);
-    L.marker(e.latlng).addTo(map)
+    L.marker(e.latlng).addTo(map1)
     .bindPopup('Place marker here? Yes or No')
   })
 }
