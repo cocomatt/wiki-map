047a0e298c15b23b5227cc48dc61931292343da1
Nic <nic.ehmayer92@gmail.com>
2018-02-10 17:52:46 +0000

    Started work on show_map view

--- routes/maps.js
+++ routes/maps.js
@@ -8,11 +8,16 @@ module.exports = (knex) => {
   router.post("/login", (req, res) => {
 
     console.log('It works!');
-    res.send('index');
+    res.render('index');
   });
 
   router.get("/", (req, res) => {
     res.render('index');
+  });
+
+
+  router.get("/:map_id", (req, res) => {
+    res.render('show_map');
   });
 
   return router;
