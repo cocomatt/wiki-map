5a322874c636e07c190b4f2611f547c6ae19dafd
Nic <nic.ehmayer92@gmail.com>
2018-02-10 01:57:26 +0000

    All markers now render to map

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,32 +3,26 @@ const newMarker = [];
 
 const latlng = {"lat":51.053126,"lng":-114.094831};
 
-function initMap(marker, description) {
+function initMap(markers, description) {
   const  map = L.map('map').setView([51.045961 , -114.069135], 13);
 
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-  L.marker(marker).addTo(map)
-    .bindPopup(description)
-    .openPopup();
+  // --- Renders all markers from markers table to map ---
+  markers.forEach(function(val, index) {
+    console.log(markers[index].latlng);
+    L.marker(markers[index].latlng).addTo(map)
+        .bindPopup(markers[index].description)
+        .openPopup();
+  })
 
   map.on('click', (e) => {
     console.log(e.latlng);
     L.marker(e.latlng).addTo(map)
     .bindPopup('Place marker here? Yes or No')
   })
-}
-
-function generateMarkers(markers) {
-  markers.forEach(function(val, index) {
-      L.marker(markers[index]).addTo(map)
-      .bindPopup(description)
-      .openPopup();
-
-  })
-  initMap();
 }
 
 
@@ -39,7 +33,7 @@ $(document).ready(function () {
     method: 'GET',
     success: function(markers) {
       console.log("success");
-      generateMarkers(markers);
+      initMap(markers);
     },
     error: function(err) {
       console.log(err);
