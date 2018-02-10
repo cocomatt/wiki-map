de80ff7876449b0827fd1403e11ee92afeebe1f2
cocomatt <matt.currie@outlook.com>
2018-02-10 19:21:35 +0000

    Able to render two different maps on same page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,28 +1,30 @@
-const newMarker = [];
-
-
-const latlng = {"lat":51.053126,"lng":-114.094831};
-
 function initMap(markers, description) {
-  const  map = L.map('map').setView([51.045961 , -114.069135], 13);      //zoom 10 first whole city in view, 20 zoom in all the way
+  const  map1 = L.map('map1').setView([51.045961 , -114.069135], 13);      //zoom 10 first whole city in view, 20 zoom in all the way
 
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    }).addTo(map1);
+
+const  map2 = L.map('map2').setView([51.045961 , -114.069135], 20);      //zoom 10 first whole city in view, 20 zoom in all the way
+
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map2);
+
 
   // --- Renders all markers from markers table to map ---
-  markers.forEach(function(val, index) {
+/*  markers.forEach(function(val, index) {
     console.log(markers[index].latlng);
     L.marker(markers[index].latlng).addTo(map)
         .bindPopup(markers[index].description)
         .openPopup();
   })
-
-  map.on('click', (e) => {
+*/
+/*  map.on('click', (e) => {
     console.log(e.latlng);
     L.marker(e.latlng).addTo(map)
     .bindPopup('Place marker here? Yes or No')
-  })
+  }) */
 }
 
 
